7eb276cb1aa9eec5a8678ea085d7254f74c29e68
Erwin Andres <erwin01@simplexi.com.ph>
2019-09-23 17:02:20 +0800

    [ADD] index component

---
+++ src/app/index/index.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Player } from '../player';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss']
+})
+export class IndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
