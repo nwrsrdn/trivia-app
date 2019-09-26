10458f6bbe3919783cab75fb89905e03a92902c7
Erwin Andres <erwin01@simplexi.com.ph>
2019-09-26 14:45:46 +0800

    [ADD] service for index component

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Highscore } from '../../classes/highscore';
+
 @Component({
     selector: 'app-index',
     templateUrl: './index.component.html',
@@ -15,11 +17,12 @@ export class IndexComponent implements OnInit {
     ngOnInit() {
     }
 
-    startGame(): void {
-        this.start = true;
-        setTimeout(() => {
-            this.router.navigate(['/questions']);
-        }, 3000);
+    startGame(name: string): void {
+        console.log(name);
+        // this.start = true;
+        // setTimeout(() => {
+        //     this.router.navigate(['/questions']);
+        // }, 3000);
     }
 
 }
